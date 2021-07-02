1c7f30bc07bf4125800ca255994ccc18cacb5624
chiyoke <chiyoke@outlook.com>
2021-07-02 12:26:28 +0800

    编写unittest功能测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.tittle
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('TO-DO' , self.browser.tittle)
+		self.fail('finish the test!')
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
