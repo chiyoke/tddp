1d0c250d3dc2359794a8f05d38d52ecac6569582
chiyoke <chiyoke@outlook.com>
2021-07-03 09:21:54 +0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,17 +1,39 @@
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+#	def tearDown(self):
+#		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
-		self.assertIn('TO-DO' , self.browser.tittle)
+		self.assertIn('To-Do' , self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		imputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			imputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+
+			)
+
+		inputbox.send_keys('Buy peacock feathers')
+		imputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertEqual(
+			any(row.text == '1:Buy peacock feathers' for row in rows)
+			)
+
 		self.fail('finish the test!')
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,7 @@
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
 
 class HomePageTest(TestCase):
-	
-	def test_root_url_resolve_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-		
+
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
