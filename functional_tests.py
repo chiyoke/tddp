d5ee73e5670c8e98da08b7be83252d8629c5abb2
chiyoke <chiyoke@outlook.com>
2021-07-03 10:16:42 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -24,14 +24,15 @@ class NewVisitorTest(unittest.TestCase):
 
 			)
 
-		inputbox.send_keys('Buy peacock feathers')
+		imputbox.send_keys('Buy peacock feathers')
 		imputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertEqual(
-			any(row.text == '1:Buy peacock feathers' for row in rows)
+			any(row.text == '1:Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 			)
 
 		self.fail('finish the test!')
