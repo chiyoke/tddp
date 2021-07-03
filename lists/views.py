845be210c6efaf241a192304e13fea056f95982b
chiyoke <chiyoke@outlook.com>
2021-07-03 23:56:27 +0800

    重定向和一个测试只做一件事

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from lists.models import Item
 
@@ -12,12 +12,9 @@ def home_page(request):
 
 
 	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
-		Item.objects.create(text=new_item_text)
-	else:
-		new_item_text = ''
 
-	return render(request, 'home.html',{
-		#'new_item_text':request.POST.get('item_text', ''),
-		'new_item_text': new_item_text,
-		})
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	
+
+	return render(request, 'home.html')
