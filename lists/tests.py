33b8bbaad1da379efb36aeb30b6220ecdfffa854
chiyoke <chiyoke@outlook.com>
2021-07-16 10:57:16 +0800

    在模板中渲染待办事项

--- lists/tests.py
+++ lists/tests.py
@@ -23,6 +23,13 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], '/')
 
+	def test_display_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+		response = self.client.get('/')
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 	
--- lists/views.py
+++ lists/views.py
@@ -12,9 +12,9 @@ def home_page(request):
 
 
 	if request.method == 'POST':
-
 		Item.objects.create(text=request.POST['item_text'])
 		return redirect('/')
 	
 
-	return render(request, 'home.html')
+	items = Item.objects.all()
+	return render(request, 'home.html',{'items': items})
