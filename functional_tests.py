ff26f89adf4ad101e8fb4c9f3cfa4ac9d757f6a8
unknown <013697@springgroup.cn>
2021-07-16 20:07:11 +0800

    练习重构

--- functional_tests.py
+++ functional_tests.py
@@ -28,31 +28,26 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
-
 			)
+		
 		#她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
 		inputbox.send_keys('Buy peacock feathers')
 		#她按回车键后，页面更新了
 		#待办事项表格中显示了“1：Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')		
 
-		inputbox = self.browser.find_element_by_tag_name('id_new_item')
+		
+		#页面中又显示了一个文本框，可以输入其它的待办事项
+		#她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
+		#伊迪丝做事很有条理
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
-		#页面中又显示了一个文本框，可以输入其它的待办事项
-		#她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
-		#伊迪丝做事很有条理
 		
 		
 		self.fail('finish the test!')
