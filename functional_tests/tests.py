8489b9a9c9f68594e92abb0339c526169cb234b4
chiyoke <chiyoke@outlook.com>
2021-08-01 11:36:13 +0800

    新的URL+增加视图到已存在的列表中+重新整理文件lists/tests.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -100,7 +100,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#弗朗西斯获得了他的唯一URL
 		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertRegexpMatches(francis_list_url, '/lists/.+')
 		self.assertNotEqual(francis_list_url, edith_list_url)
 
 		#这个页面还是没有伊迪斯的清单
--- lists/tests.py
+++ lists/tests.py
@@ -49,10 +49,10 @@ class ListViewTest(TestCase):
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
         other_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=other_list)
-        Item.objects.create(text='itemey 2', list=other_list)
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('f/lists/{correct_list.id}/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
@@ -74,7 +74,41 @@ class NewListTest(TestCase):
 	def test_redirect_after_POST(self):
 		##/new后面不加斜线，作者HJWP一般不在修改数据库的“操作”后加斜线
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		new_list = List.objects.first()
 		#self.assertEqual(response.status_code, 302)
 		#self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 		##上面两个assertEqual断言可以精简成以下一条
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+class NewItemTest(TestCase):
+
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post(
+			f'/lists/{correct_list.id}/add_item',
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+
+
+	def test_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(
+			f'/lists/{correct_list.id}/add_item',
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.get(f'/lists/{correct_list.id}/')
+		self.assertEqual(response.context['list'], correct_list)
